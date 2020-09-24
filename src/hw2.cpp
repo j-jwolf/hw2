a5111a2365f0116a47e22c8971f18d10fa70a114
j-jwolf <jjwolf@noctrl.edu>
2020-09-24 18:16:40 -0500

    added a few private methods for modularization

--- src/hw2.cpp
+++ src/hw2.cpp
@@ -23,15 +23,27 @@ using namespace std;
  * Remember, this is all one file. Do NOT make seperate .h and .cpp
  * files for class LexAnalyzer
  *
- * Main issue so far:
- * 	- The last line in tokens.txt is being read, pushed onto the vector correctly and even being put into its associated string. However, it's not
- * 		being printed out from the map iterator properly. It is in the map though, it can print via _tokenMap["!"]
- * 		-- Fixed, it's sorted so it isn't in the last index, idiot
+ * Issues:
+ * 	-Was getting an error for map::iterator when trying to put a method below isString, worked fine after moving it. Find the source, maybe a spacing
+ * 	 or brackets/parenthesis issue?
  *
  * Notes:
- * 	-Ask if we should/can implement a default constructor for LexAnalyzer. It's a bit pointless but it really wouldn't hurt
- * 		--Just  have to remember that you'd need to have a set map method that has the istream reference to read from
- *  -//
+ * 	-Still need to implement:
+ * 		-error display and codes
+ * 		-check if(identifier proceeds integer/string)
+ * 		-write out (need to figure out how ostream works?)
+ * 		-basic UI
+ * 		-rework isString and isNumber, should return string "t_int" or "t_string" rather than true/false?
+ * 			-or at least implement that in the scanFile method
+ *
+ *================================================================== Reference ===========================================================================
+ * I wasn't sure how to handle istream and ostream objects. This is a small 'reference' of sorts to the website that I've been looking at how to handle
+ * certain classes and methods in c++: www.cplusplus.com/reference/
+ *
+ * I don't know if it's entirely necessary but I would rather have a source cited than not
+ *========================================================================================================================================================
+ *
+ *
  *
  *
  */
@@ -66,6 +78,12 @@ private:
 	 *
 	 * @return null
 	 */
+	bool isString(string string)
+	{
+		int length = string.length();
+		if(string[0] == '"' && string[length-1] == '"') {return true;}
+		return false;
+	}
 	void testPrint()
 	{
 		cout << "keys" << endl << endl;
@@ -84,7 +102,11 @@ private:
 	}
 	/*
 	 * !!! Delete before turning in !!!
-	 * This is a test method to write all of the keys and tokens from map to confirm if they were read correctly from file
+	 * This is a test method to write all of the keys and tokens from map to confirm if they were read correctly from file as well as checking the
+	 * confirming that the strings write properly
+	 *
+	 * This is mostly obsolete, the method that worked with this was removed. The only reason that it's useful still is being able to reference a txt file
+	 * that has the strings saved, rather than having to scroll through a console
 	 *
 	 * @return null
 	 */
@@ -108,7 +130,46 @@ private:
 		}
 		outfile.close();
 	}
+	vector<string> splitLine(string s)
+	{
+		vector<string> inputs;
+		int strLength = s.length(), count = 0;
+		string temp = "";
+		while(count < strLength)
+		{
+			if(s[count] != ' ') {temp += s[count];}
+			else
+			{
+				if(temp != "")
+				{
+					inputs.push_back(temp);
+					temp = "";
+				}
+			}
+		}
+		if(temp != "") {inputs.push_back(temp);}
+		return inputs;
+	}
+	string getOutLine(string source)
+	{
+		string out, token;
+		return out;
+	}
+	string isKey(string s)
+	{
+		std::map<string, string>::iterator mitr = _tokenMap.begin();
+		string temp = "";
+		while(mitr != _tokenMap.end() && temp != s)
+		{
+			temp = mitr->first;
+			if(temp == s) {return mitr->second;}
+			mitr++;
+		}
+		return "false";
+	}
 public:
+	LexAnalyzer(){}
+	void setMap(istream&); // document this, this is for if the default constructor is used rather than the n-argument
 	LexAnalyzer(istream&);
 	void scanFile(istream&, ostream&);
 	void test() {testOut();} // calls for test out, redundant. remove this for easier testing
@@ -127,7 +188,7 @@ public:
 LexAnalyzer::LexAnalyzer(istream& infile)
 {
 	string temp;
-	if(!infile) {exit(-1);}
+	if(!infile) {exit(-1);} // change, need sentinel loop structure
 	while(getline(infile, temp))
 	{
 		string token = "", key = "";
@@ -178,22 +239,43 @@ LexAnalyzer::LexAnalyzer(istream& infile)
  */
 void LexAnalyzer::scanFile(istream& infile, ostream& outfile)
 {
+	/*
+	 * !!! You're forgetting to check for ints (str len == 1 && isNumber), strings (make a separate method isString, vector<char>, push single quote onto
+	 * vector, then pop if encountered again? basically, treat it like a stack) and the keywords/assignments !!!
+	 */
 	vector<string> linesout;
+	string temp;
+	getline(infile, temp);
 	while(!infile.eof())
 	{
-		string temp;
-		getline(infile, temp);
-		vector<char> symbols;
-		char letter = '0';
-		int length = temp.length(), count = 0;
-		while(count < length && letter != ' ')
+		/*
+		 * Layout:
+		 * 1- vector<string> sline = splitLine(temp)
+		 * 2- for(len(sline)) -- MAKE SURE TO CHECK IF IT'S A NUMBER HERE. DON'T KEEP MAKING LOOPS THAT TRAVERSE THE SAME THING
+		 * 	if((sline[i])[0] is a number) push a string signifying that it's an error and why it is
+		 * 		-- Need to ask about this tomorrow or friday during office hours
+		 * 	else linesout.push(sline[i])
+		 * 3- compare each of the lines to the token text file
+		 * 	-- need to check the string in vector first
+		 * 		if(linesout.substr(0, 5) != "Error:") {check(string)}
+		 */
+		vector<string> sline = splitLine(temp);
+		int vecSize = sline.size();
+		for(int i = 0; i < vecSize; i++)
 		{
-			if(count == 0 && isNumber(temp[0])) // if starts with number and does not only contain numbers, throw error
-			{
-				//
-			}
+			if(isNumber((sline[i])[0])) {linesout.push_back("Error: cannot have integer start variable name");}
+			else {linesout.push_back(sline[i]);}
 		}
 	}
+}
+bool isNumber(char letter)
+{
+	string nums = "1234567890";
+	for(int i = 0; i < 10; i++)
+	{
+		if(letter == nums[i]) {return false;}
+	}
+	return true;
 }
 int main()
 {
@@ -215,13 +297,46 @@ int main()
 	}
 	fb.close();*/
 
-	filebuf fb;
+	/*filebuf fb;
 	fb.open("tokens.txt", std::ios::in);
 	istream is(&fb);
 	if(!is.eof()) {cout << "not eof";} // should be this
 	else {cout << "is eof";}
 	cout << endl;
 	LexAnalyzer la(is);
-	la.test();
+	la.test();*/
+
+	// this is for testing how to stack the chars and work with them. c++ doesn't have a split method so you'll have to get creative
+	/*string test = "this is a  test    to 2see if  it works";
+	int length = test.length(), count = 0;
+	string line = "";
+	vector<string> inputs;
+	while(count < length)
+	{
+		if(test[count] != ' ') {line += test[count];}
+		else
+		{
+			if(test[count] == ' ' && line != "")
+			{
+				inputs.push_back(line);
+				line = "";
+			}
+		}
+		count++;
+	}
+	if(line != "") {inputs.push_back(line);}
+	int incount = inputs.size();
+	for(int i = 0; i < incount; i++)
+	{
+		if(isNumber((inputs[i])[0]))
+		{
+			cout << inputs[i] << endl;
+		}
+		else {cout << "Error: invalid variable name -- cannot start with a number" << endl;}
+	}*/
+
+	// The code above splits it and the isNumber method works properly
+
+
 	return 0;
 }
