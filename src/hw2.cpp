6068bb014907727517b9579a17f6fb32f5f46a0c
j-jwolf <jjwolf@noctrl.edu>
2020-09-28 10:48:38 -0500

    large change, rewrote scan file and added/changed helper methods

--- src/hw2.cpp
+++ src/hw2.cpp
@@ -15,6 +15,13 @@
 using namespace std;
 
 /*
+ *
+ * Input.txt works in current configuration. The issue was that you were not checking for tabs in the chars being passed in
+ * 	- You aren't properly checking strings though. Fix it. Either have a counter that ++ on open and -- on close OR vector<char> push('"') on open, pop() on close
+ * 	- try the integers first before you start ruining stuff please
+ * 		-- getting there. Isn't recognizing integers nor is it recognizing = sign. It's probably the string read in checkLine that isn't being added to correctly
+ * 		all in all, should be easy-ish fixes so get them done please
+ *
  * Compiler part 1 -- Lexical analyzer
  *
  * read in a file of source code, determining if the words are a part
@@ -29,12 +36,13 @@ using namespace std;
  *
  * Notes:
  * 	-Still need to implement:
- * 		-error display and codes
+ * 		-error display and codes -- Added, needs testing now
  * 		-check if(identifier proceeds integer/string)
- * 		-write out (need to figure out how ostream works?)
+ * 		-write out (need to figure out how ostream works?) same as ofstream essentially? or does passing an ofstream work with inheritance?
  * 		-basic UI
  * 		-rework isString and isNumber, should return string "t_int" or "t_string" rather than true/false?
- * 			-or at least implement that in the scanFile method
+ * 			-or at least implement that in the scanFile method -- opted for this, needs testing
+ *
  *
  *================================================================== Reference ===========================================================================
  * I wasn't sure how to handle istream and ostream objects. This is a small 'reference' of sorts to the website that I've been looking at how to handle
@@ -73,32 +81,37 @@ private:
 		return false;
 	}
 	/*
-	 * !!! Delete before turning in !!!
-	 * This is a test method that prints all of the keys and tokens in the map _tokenMap
+	 * pre: called to check if the given string is syntactically a string containing two quotation marks
+	 * post: returns boolean confirming if the string is a string in the source code
 	 *
-	 * @return null
+	 * @return true if given string from source code has correct syntax, else false
 	 */
-	bool isString(string string)
+	string isString(string string) // needs a complete rewrite
 	{
-		int length = string.length();
-		if(string[0] == '"' && string[length-1] == '"') {return true;}
-		return false;
+		int length = string.length(), count = 0, quoteCount = 0;
+		vector<char> cVec;
+		while(count < length && quoteCount < 3)
+		{
+			char c = string[count];
+			if(c == '"') {quoteCount++;}
+			count++;
+		}
+		if(quoteCount == 0) {return "not string";}
+		else if(quoteCount > 2) {return "invalid";}
+		else {return "valid";}
 	}
-	void testPrint()
+	/*
+	 * !!! either use this or delete it !!!
+	 */
+	int countQmarks(string source)
 	{
-		cout << "keys" << endl << endl;
-		for(std::map<string, string>::iterator mitr = _tokenMap.begin(); mitr != _tokenMap.end(); mitr++)
+		int qmarks = 0, length = source.length();
+		for(int i = 0; i < length; i++)
 		{
-			/*cout << mitr->first << ", " << mitr->second << endl;
-			string line = mitr->first + " " + mitr->second;
-			outfile << line << endl;*/
-			cout << mitr->first << endl;
+			char c = source[i];
+			if(c == '"') {qmarks++;}
 		}
-		cout << endl << "tokens" << endl << endl;
-		for(std::map<string, string>::iterator mitr = _tokenMap.begin(); mitr != _tokenMap.end(); mitr++)
-		{
-			cout << mitr->second << endl;
-		}
+		return qmarks;
 	}
 	/*
 	 * !!! Delete before turning in !!!
@@ -130,6 +143,26 @@ private:
 		}
 		outfile.close();
 	}
+	/*
+	 * !!! delete before turning in !!!
+	 *
+	 * @return null
+	 */
+	void testPrint()
+	{
+		for(std::map<string, string>::iterator mitr = _tokenMap.begin(); mitr != _tokenMap.end(); mitr++)
+		{
+			cout << "First: " << mitr->first << endl << "Second: " << mitr->second << endl << "------------------------" << endl;
+		}
+	}
+	/*
+	 * pre: have a string that needs to be split at char ' '
+	 * post: returns a vector containing the strings of the split original
+	 *
+	 * @param s: string to split
+	 *
+	 * @return vector containing the split string
+	 */
 	vector<string> splitLine(string s)
 	{
 		vector<string> inputs;
@@ -146,26 +179,107 @@ private:
 					temp = "";
 				}
 			}
+			count++;
 		}
 		if(temp != "") {inputs.push_back(temp);}
 		return inputs;
 	}
-	string getOutLine(string source)
+	/*
+	 * !!! THIS NEEDS TO BE MOVED TO PRIVATE AFTER TESTING !!!
+	 *
+	 * pre: have a line that needs to be checked for syntax
+	 * post: vector containing the tokens and potential error found in the string
+	 *
+	 * @param line: line that is to be checked for syntax
+	 *
+	 * @return vector containing tokens and potential error; there will only be 1 error at most
+	 */
+	vector<string> checkLine(string line)
 	{
-		string out, token;
-		return out;
-	}
-	string isKey(string s)
-	{
-		std::map<string, string>::iterator mitr = _tokenMap.begin();
-		string temp = "";
-		while(mitr != _tokenMap.end() && temp != s)
+		vector<string> tokens;
+		string read = "", error = "";
+		int length = line.length(), count = 0;
+		bool test = false;
+		while(!test)
 		{
-			temp = mitr->first;
-			if(temp == s) {return mitr->second;}
-			mitr++;
+			char temp = line[count];
+			if(temp == ' ' || temp == '\t')
+			{
+				count++;
+			}
+			else {test = true;}
 		}
-		return "false";
+		while(count < length && error == "")
+		{
+			char c = line[count];
+			string temp;
+			cout << "C: " << c << endl;
+			if(c != ' ' && c != '\t') {read += c;}
+			cout << "read after adding c: " << read << endl;
+			if(_tokenMap[to_string(c)] != "")
+			{
+				cout << "c: " << c << " matches token " << _tokenMap[to_string(c)] << endl;
+				temp = _tokenMap[to_string(c)]+" : "+c;
+				tokens.push_back(temp);
+			}
+			else
+			{
+				cout << "Test: " << _tokenMap[read] << endl << "Read: " << read << endl;
+				if(_tokenMap[read] != "")
+				{
+					cout << "read: " << read << " matches token " << _tokenMap[read] << endl;
+					temp = _tokenMap[read]+" : "+read;
+					tokens.push_back(temp);
+					read = "";
+				}
+				cout << "added to read, read: " << read << " has no match" << endl;
+				cout << "force test: read: " << read << endl << "token[read]: " << _tokenMap[read] << endl;
+				cout << "token[begin]: " << _tokenMap["begin"] << endl;
+			}
+			if(temp != "" && read != "")
+			{
+				if(isNumber(read[0]))
+				{
+					// integer check
+					int length = read.length(), icnt = 0;
+					if(length > 1)
+					{
+						icnt = 1;
+						while(icnt < length)
+						{
+							if(!isNumber(read[icnt])) {error = "Error: cannot have identifier start with an integer";} // error
+							else {icnt++;} // is still int
+						}
+						if(error == "") {tokens.push_back("t_int : "+read); read = "";} // is syntactically an integer
+					}
+				}
+				else
+				{
+					// string check
+					int length = read.length(), strCount = 0, qcnt = 0;
+					while(strCount < length && error == "")
+					{
+						char str_c = read[strCount];
+						if(str_c == '"') {qcnt++;}
+						if(qcnt > 2) {error = "Error: cannot use a quotation mark in an identifier";} // string/identifier error
+					}
+					if(error == "" && qcnt == 2) {tokens.push_back("t_str : "+read); read = "";} // is syntactically a string
+					else if(error == "" && qcnt != 2) {error = "Error: cannot use quotation mark in an identifier";} // string/identifier error
+					else if(error == "" && qcnt == 0)
+					{
+						strCount = 0;
+						while(strCount < length && error == "")
+						{
+							char str_c = read[strCount];
+							strCount++;
+						}
+						if(error == "") {tokens.push_back("t_id : "+read); read = "";}
+					}
+				}
+			}
+			count++;
+		}
+		return tokens;
 	}
 public:
 	LexAnalyzer(){}
@@ -188,8 +302,8 @@ public:
 LexAnalyzer::LexAnalyzer(istream& infile)
 {
 	string temp;
-	if(!infile) {exit(-1);} // change, need sentinel loop structure
-	while(getline(infile, temp))
+	getline(infile, temp);
+	while(!infile.eof())
 	{
 		string token = "", key = "";
 		vector<char> tokenvec;
@@ -199,14 +313,14 @@ LexAnalyzer::LexAnalyzer(istream& infile)
 		while(count < length && index != ' ')
 		{
 			index = temp[count];
+			//cout << "here?" << endl;
 			if(index != ' ')
 			{
-				count++;
 				tokenvec.push_back(index);
 				//cout << index << endl;
 			}
+			count++;
 		}
-		count++;
 		index = '0';
 		while(count < length && index != ' ')
 		{
@@ -219,12 +333,13 @@ LexAnalyzer::LexAnalyzer(istream& infile)
 		for(int i = 0; i < tokenLength; i++) {token.push_back(tokenvec[i]);}
 		//cout << "===========================" << endl;
 		for(int i = 0; i < keyLength; i++) {key.push_back(keyvec[i]);}
-
 		_tokenMap[key] = token;
 		tokenvec.clear();
 		keyvec.clear();
+		getline(infile, temp);
 	}
-	//testPrint();
+	testPrint();
+	cout << endl << "--------" << endl << "Test: tokenMap[begin]: " << _tokenMap["begin"] << endl;
 }
 /*
  * pre:
@@ -237,106 +352,54 @@ LexAnalyzer::LexAnalyzer(istream& infile)
  *
  * @return null
  */
-void LexAnalyzer::scanFile(istream& infile, ostream& outfile)
+void LexAnalyzer::scanFile(istream& infile, ostream& outfile) // this needs a complete rewrite. it's fucking trash
 {
-	/*
-	 * !!! You're forgetting to check for ints (str len == 1 && isNumber), strings (make a separate method isString, vector<char>, push single quote onto
-	 * vector, then pop if encountered again? basically, treat it like a stack) and the keywords/assignments !!!
-	 */
-	vector<string> linesout;
-	string temp;
+	string temp, error = "";
 	getline(infile, temp);
-	while(!infile.eof())
+	vector<string> toWrite;
+	while(!infile.eof() && error == "")
 	{
-		/*
-		 * Layout:
-		 * 1- vector<string> sline = splitLine(temp)
-		 * 2- for(len(sline)) -- MAKE SURE TO CHECK IF IT'S A NUMBER HERE. DON'T KEEP MAKING LOOPS THAT TRAVERSE THE SAME THING
-		 * 	if((sline[i])[0] is a number) push a string signifying that it's an error and why it is
-		 * 		-- Need to ask about this tomorrow or friday during office hours
-		 * 	else linesout.push(sline[i])
-		 * 3- compare each of the lines to the token text file
-		 * 	-- need to check the string in vector first
-		 * 		if(linesout.substr(0, 5) != "Error:") {check(string)}
-		 */
-		vector<string> sline = splitLine(temp);
-		int vecSize = sline.size();
-		for(int i = 0; i < vecSize; i++)
+		vector<string> tokens = checkLine(temp);
+		// test print
+		int testSize = tokens.size();
+		for(int i = 0; i < testSize; i++)
 		{
-			if(isNumber((sline[i])[0])) {linesout.push_back("Error: cannot have integer start variable name");}
-			else {linesout.push_back(sline[i]);}
+			cout << "In scanFile, tokens[" << i << "]: " << tokens[i] << endl; // made it here!
 		}
+		int vecSize = tokens.size(), count = 0;
+		while(count < vecSize)
+		{
+			string copy = tokens[count];
+			cout << "copy string: " << copy << endl;
+			toWrite.push_back(copy);
+			count++;
+		}
+		string errorCheck = tokens[vecSize-1];
+		if(errorCheck.substr(0, 5) == "Error:") {error = errorCheck;}
+		getline(infile, temp);
+		cout << "Next line: " << temp << endl;
 	}
-}
-bool isNumber(char letter)
-{
-	string nums = "1234567890";
-	for(int i = 0; i < 10; i++)
+	int linesout = toWrite.size(), lnCount = 0;
+	while(lnCount < linesout)
 	{
-		if(letter == nums[i]) {return false;}
+		outfile << toWrite[lnCount] << endl;
+		lnCount++;
 	}
-	return true;
+	outfile.flush();
+	if(error != "") {cout << error << endl;}
 }
 int main()
 {
-	/*
-	 * This works for istream, so this is commented out to keep as a
-	 * reference while you work -- make sure to delete before turning
-	 * this in
-	 */
-	// this works for opening istream
-	/*filebuf fb;
-	fb.open("tokens.txt", std::ios::in);
-	istream is(&fb);
-	// test out reading
-	string temp;
-	while(!is.eof())
-	{
-		getline(is, temp);
-		cout << temp << endl;
-	}
-	fb.close();*/
-
-	/*filebuf fb;
-	fb.open("tokens.txt", std::ios::in);
-	istream is(&fb);
-	if(!is.eof()) {cout << "not eof";} // should be this
-	else {cout << "is eof";}
-	cout << endl;
-	LexAnalyzer la(is);
-	la.test();*/
-
-	// this is for testing how to stack the chars and work with them. c++ doesn't have a split method so you'll have to get creative
-	/*string test = "this is a  test    to 2see if  it works";
-	int length = test.length(), count = 0;
-	string line = "";
-	vector<string> inputs;
-	while(count < length)
-	{
-		if(test[count] != ' ') {line += test[count];}
-		else
-		{
-			if(test[count] == ' ' && line != "")
-			{
-				inputs.push_back(line);
-				line = "";
-			}
-		}
-		count++;
-	}
-	if(line != "") {inputs.push_back(line);}
-	int incount = inputs.size();
-	for(int i = 0; i < incount; i++)
-	{
-		if(isNumber((inputs[i])[0]))
-		{
-			cout << inputs[i] << endl;
-		}
-		else {cout << "Error: invalid variable name -- cannot start with a number" << endl;}
-	}*/
-
-	// The code above splits it and the isNumber method works properly
-
-
+	//==================================================================================================================================================================
+	ifstream infile;
+	ofstream outfile;
+	string tokenfn = "tokens.txt", inputfn = "input2.txt", outputfn = "outfile2.txt";
+	infile.open(tokenfn);
+	LexAnalyzer lex(infile);
+	infile.close();
+	infile.open(inputfn);
+	outfile.open(outputfn);
+	lex.scanFile(infile, outfile);
+	outfile.close();
 	return 0;
 }
